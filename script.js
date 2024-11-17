5755d4b0a6e267cfe89e0cd8fc1d3cbcfd333c62
Archi-755 <mangukiyabhavy@gmail.com>
2024-11-17 20:06:06 +0530

    updated all files

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('animate-box').addEventListener('click', function() {
+    this.style.backgroundColor = this.style.backgroundColor === 'rgb(52, 152, 219)' ? '#e74c3c' : '#3498db';
+});
